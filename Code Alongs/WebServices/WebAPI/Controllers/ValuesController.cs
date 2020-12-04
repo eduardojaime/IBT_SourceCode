a457608167259c17ff9205a30ec20d1dd3b0dab9
Eduardo Jaime <eduardojaime86@gmail.com>
2020-12-03 19:23:45 -0500

    Dec 3 - Added WebAPI

---
+++ Code Alongs/WebServices/WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EFDataAccessLibrary;
+
+namespace WebAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET/POST/PATCH/DELETE
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpGet]
+        [ActionName("GetFirstStudent")]
+        public Student MyCustomMethod()
+        {
+            StudentsModel dbContext = new StudentsModel();
+
+            return dbContext.Students.First();
+        }
+
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        public List<Student> GetStudentsList()
+        {
+            StudentsModel dbContext = new StudentsModel();
+
+            return dbContext.Students.ToList();
+        }
+
+        [NonAction]
+        public bool SendEmail()
+        {
+            return true;
+        }
+    }
+}
